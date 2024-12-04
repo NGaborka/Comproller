81c32b78f2dea1bd8e0a82b6fac208e6fa28c511
Kiss Bercel <109385729+ElectronSama@users.noreply.github.com>
2024-12-04 12:32:18 +0200

    szerepkörök kezelése

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,36 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+           
     }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Blade::if('role', function (...$roles) {
+            $user = auth()->user();
+            if ($user && $user->role === 'admin') {
+                return true;
+            }
+            return $user && in_array($user->role, $roles);
+        });
+        Blade::if('admin', function () {
+            $user = auth()->user();
+            return $user && $user->role === 'admin';
+        });
+
+        Blade::if('hr', function () {
+            $user = auth()->user();
+            return $user && $user->role === 'hr';
+        });
+
+        Blade::if('pu', function () {
+            $user = auth()->user();
+            return $user && $user->role === 'pu';
+        });
     }
 }
