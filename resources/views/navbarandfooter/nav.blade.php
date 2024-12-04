f41e7a84b0367a6a405bba96bd6ec00571cacdce
Kiss Bercel <109385729+ElectronSama@users.noreply.github.com>
2024-12-04 12:48:33 +0200

    Update nav.blade.php

--- resources/views/navbarandfooter/nav.blade.php
+++ resources/views/navbarandfooter/nav.blade.php
@@ -1,97 +1,63 @@
 <style>
-.button {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 6px 12px;
-    gap: 8px;
-    height: 36px;
-    width: 145px;
-    border: none;
-    background: #5e41de33;
-    border-radius: 20px;
-    cursor: pointer;
-}
-
-.label {
-    line-height: 20px;
-    font-size: 17px;
-    color: #5D41DE;
-    font-family: sans-serif;
-    letter-spacing: 1px;
-}
-
-.button:hover {
-    background: #5e41de4d;
-}
-
-.button:hover .svg-icon {
-    animation: spin 2s linear infinite;
-}
-
-@keyframes spin {
-    0% {
-        transform: rotate(0deg);
+  nav{ background-color: rgb(162, 175, 255); }
+  .btn-outline-primary{ color: azure; transition: 0.5s; border-color: azure;}
+  .btn-outline-primary:hover{ color: rgb(31, 47, 84);background-color: azure; letter-spacing: 5px; transition: 0.5s; border-color: azure; }
+  .nav-link { color: white; transition: 0.5s; }
+  .nav-link:hover{ color: rgb(31, 47, 84); transition: 0.5s; border-bottom: 1px solid rgb(31, 47, 84); padding-bottom: 2px; }
+  .dropdown-icon-btn {
+      background: none;
+      border: none;
+      padding: 0;
+      margin: 0;
+      cursor: pointer;
     }
-    100% {
-        transform: rotate(360deg);
+    .dropdown-icon-btn .bi {
+      font-size: 24px;
+      color: azure;
+      transition: 0.5s;
     }
-}
-
-.svg-icon {
-    width: 20px;
-    height: 20px;
-}
-#userDropdown img:hover {
-    transform: scale(1.2);  
-    transition: transform 0.5s ease;  
-}
+    .dropdown-icon-btn .bi:hover {
+      color: rgb(31, 47, 84);
+      font-size: 30px;
+    }
+    #navbarNav
+    { margin-left: 15px; }
 </style>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a href="/" class="button nav-link">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" height="30" fill="none" class="svg-icon">
-                <g stroke-width="1.5" stroke-linecap="round" stroke="#5d41de">
-                    <circle r="2.5" cy="10" cx="10"></circle>
-                    <path fill-rule="evenodd" d="m8.39079 2.80235c.53842-1.51424 2.67991-1.51424 3.21831-.00001.3392.95358 1.4284 1.40477 2.3425.97027 1.4514-.68995 2.9657.82427 2.2758 2.27575-.4345.91407.0166 2.00334.9702 2.34248 1.5143.53842 1.5143 2.67996 0 3.21836-.9536.3391-1.4047 1.4284-.9702 2.3425.6899 1.4514-.8244 2.9656-2.2758 2.2757-.9141-.4345-2.0033.0167-2.3425.9703-.5384 1.5142-2.67989 1.5142-3.21831 0-.33914-.9536-1.4284-1.4048-2.34247-.9703-1.45148.6899-2.96571-.8243-2.27575-2.2757.43449-.9141-.01669-2.0034-.97028-2.3425-1.51422-.5384-1.51422-2.67994.00001-3.21836.95358-.33914 1.40476-1.42841.97027-2.34248-.68996-1.45148.82427-2.9657 2.27575-2.27575.91407.4345 2.00333-.01669 2.34247-.97026z" clip-rule="evenodd"></path>
-                </g>
-            </svg>
-            <span class="label">Comproller</span>
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto">
-                @guest
-                <li class="nav-item">
-                    <a class="nav-link" href="/">Kezdőlap</a>
-                </li>
-                @endguest
-                @auth
-                <li class="nav-item">
-                    <a class="nav-link" href="/hr">Menü</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/penzugy">Menü</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuButton2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Eszköztár2
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-                @endauth
-            </ul>
-            @guest
-            <div class="d-flex ms-auto">
-                <a href="/bejelentkezes" class="btn btn-outline-primary">Bejelentkezés</a>
-            </div>
-            @endguest
-        </div>
+<nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <a class="btn btn-outline-primary rounded-pill" href="/">Comproller</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        @guest
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="/">Kezdőlap</a>
+        </li>
+        @endguest
+        @role('hr')
+        <li class="nav-item">
+          <a class="nav-link" href="#">HR</a>
+        </li>
+        @endrole
+        @role('pu')
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pénzügy</a>
+        </li>
+        @endrole
+      </ul>
     </div>
+    <div class="dropdown">
+      <button class="dropdown-icon-btn" id="iconDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="bi bi-arrow-down-circle"></i>
+      </button>
+      <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="iconDropdown">
+        <li><a class="dropdown-item" href="#">Profil</a></li>
+        <li><a class="dropdown-item" href="#">Beállítások</a></li>
+        <li><a class="dropdown-item" href="#">Kijelentkezés</a></li>
+      </ul>
+    </div>    
+  </div>
 </nav>
+</body>
