e1e8c4b78dd5ef7f7929a83220af5d1f90e9d446
Papp Gyula <144594449+guts7777@users.noreply.github.com>
2024-12-26 17:50:56 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DolgozoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/rolunk', function () {
+    return view('rolunk');
+});
+
+Route::get('/kapcsolat', function () {
+    return view('kapcsolat');
+});
+
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get('/dolgozok', [DolgozoController::class, 'index']);
